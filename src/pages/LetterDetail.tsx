1fcc1e1e79b3949d1a0b012a82a1a4da26e790f3
KANG0417 <ipia99@naver.com>
2023-12-26 17:59:44 +0900

    remove 컴포넌트 삭제

--- src/pages/LetterDetail.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LetterDetail = () => {
-  return <div>LetterDetail</div>;
-};
-
-export default LetterDetail;
--- src/pages/LetterWrite.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LetterWrite = () => {
-  return <div>LetterWrite</div>;
-};
-
-export default LetterWrite;
