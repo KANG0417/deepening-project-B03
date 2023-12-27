3c145c2dab817b626835723182fc0766e1bc5b5a
HONGSANGYOON <jay182@naver.com>
2023-12-27 19:57:12 +0900

    Feature: 버튼토글기능, 캘린더 버튼별 on,off기능

--- src/components/writingletter/OptionBox.tsx
+++ src/components/writingletter/OptionBox.tsx
@@ -1,19 +1,45 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-
 import "react-datepicker/dist/react-datepicker.css";
 import Calendar from "./Calendar";
 
 const OptionBox = () => {
+  const [isSendImmediate, setIsSendImmediate] = useState<boolean>(true);
+  const [isImmediate, setIsImmediate] = useState<boolean>(true);
+
+  //바로보내기 or 예약보내기 버튼 활성화
+  const SendToggleButton = (isSendImmediate: boolean) => {
+    setIsSendImmediate(isSendImmediate);
+  };
+  //공개 or 비공개 버튼 활성화
+  const ToggleButton = (isImmediate: boolean) => {
+    setIsImmediate(isImmediate);
+  };
+
   return (
     <SOptionBox>
-      <Calendar />
+      <SPublicButton
+        onClick={() => SendToggleButton(true)}
+        active={isSendImmediate}
+      >
+        바로보내기
+      </SPublicButton>
+      <SPublicButton
+        onClick={() => SendToggleButton(false)}
+        active={!isSendImmediate}
+      >
+        예약보내기
+      </SPublicButton>
+      {isSendImmediate ? null : <Calendar />}
       <h3>공개/비공개</h3>
-      <SPublicButton>공개</SPublicButton>
-      <SPublicButton>익명으로 공개</SPublicButton>
+      <SPublicButton onClick={() => ToggleButton(true)} active={isImmediate}>
+        공개
+      </SPublicButton>
+      <SPublicButton onClick={() => ToggleButton(false)} active={!isImmediate}>
+        익명으로 공개
+      </SPublicButton>
       <h3>발신할 곳</h3>
-      <input placeholder="보낼곳을 적어주세요"></input>
+      <input placeholder="보낼곳을 적어주세요" />
     </SOptionBox>
   );
 };
@@ -24,7 +50,10 @@ const SOptionBox = styled.div`
   height: 60vh;
 `;
 
-const SPublicButton = styled.button`
+const SPublicButton = styled.button<{ active: boolean }>`
   border: 0.1px solid;
+  background-color: ${(props) => (props.active ? "#00f" : "#fff")};
+  color: ${(props) => (props.active ? "#fff" : "#000")};
 `;
+
 export default OptionBox;
