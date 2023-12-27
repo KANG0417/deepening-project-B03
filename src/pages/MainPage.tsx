5c52a30395e569755d7b60fca639aa7ba6bfe5aa
KANG0417 <ipia99@naver.com>
2023-12-27 10:12:08 +0900

    add 메인페이지 컴포넌트 분리중

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,27 @@
 import styled from "styled-components";
 
 const MainPages = () => {
-  return <SMainWrapper>MainPages</SMainWrapper>;
+  
+
+  return (
+    <SMainWrapper>
+      <ul>
+        필터
+        <li>최신순</li>
+        <li>오래된순</li>
+      </ul>
+      <SLetterList>
+        <li>제목: </li>
+        <li>내용: </li>
+        <li>태그: </li>
+        <li>좋아요: 123</li>
+      </SLetterList>
+    </SMainWrapper>
+  );
 };
 
 export default MainPages;
 
 const SMainWrapper = styled.div``;
+
+const SLetterList = styled.ul``;
