473a0428a892fd3bb20f2dfa5a7187c38c1fc703
HONGSANGYOON <jay182@naver.com>
2023-12-28 22:03:47 +0900

    병합하기

---
+++ src/components/writingLetter/WritingField.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+const WritingField = () => {
+  const [letterIndex, setLetterIndex] = useState<string>("");
+
+  const LetterIndexHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setLetterIndex(e.target.value);
+    console.log(letterIndex);
+  };
+
+  return (
+    <STextArea
+      onChange={LetterIndexHandler}
+      placeholder="글을 작성해주세요(1000자 제한)"
+    />
+  );
+};
+
+const STextArea = styled.textarea`
+  width: 30%;
+  height: 60vh;
+  resize: none;
+`;
+
+export default WritingField;
