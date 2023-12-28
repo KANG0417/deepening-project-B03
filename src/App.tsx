c36417f21aeb0e8ee535a3b9dd95d267a2127867
KANG0417 <ipia99@naver.com>
2023-12-28 18:30:21 +0900

    update 사용자 정보 유지 없으면 로그인 페이지 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Router from "./shared/Router";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+      navigate("/login");
+    }
+  });
+
   return <Router />;
 }
 
