09f73ebaf8ffeb76d3c077b9259b52d28495f350
HONGSANGYOON <jay182@naver.com>
2023-12-31 19:09:30 +0900

    페이지, 컴포넌트 별 타입 정리

---
+++ src/type/joinPage/writingLetter/optionBox.ts
@@ -0,0 +1,13 @@
+export type TOptionBoxProps = {
+  initialTags: string[];
+  tags: string[];
+  setTags: React.Dispatch<React.SetStateAction<string[]>>;
+  isSendImmediate: boolean;
+  setIsSendImmediate: React.Dispatch<React.SetStateAction<boolean>>;
+  isPublic: boolean;
+  setIsPublic: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedDate: Date | null;
+  setSelectedDate: React.Dispatch<React.SetStateAction<Date | null>>;
+  selectedTag: string | null;
+  setSelectedTag: React.Dispatch<React.SetStateAction<string | null>>;
+};
---
+++ src/type/joinPage/writingLetter/writingLetter.ts
@@ -0,0 +1,4 @@
+export type WritingLetterProps = {
+  letterIndex: string;
+  setLetterIndex: React.Dispatch<React.SetStateAction<string>>;
+};
