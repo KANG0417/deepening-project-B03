b7182b8656f0e1f8b836ac3fd660a3ca67de85f0
HONGSANGYOON <jay182@naver.com>
2023-12-31 17:45:17 +0900

    Featrue: 태그버튼기능 활성

--- src/components/writingLetter/OptionBox.tsx
+++ src/components/writingLetter/OptionBox.tsx
@@ -1,47 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import "react-datepicker/dist/react-datepicker.css";
 import Calendar from "./Calendar";
+import WritingField from "./WritingField";
 
 const OptionBox = () => {
+  const initialTags = ["신년인사", "연인에게", "익명에게"];
+  const [tags, setTags] = useState<string[]>(initialTags);
   const [isSendImmediate, setIsSendImmediate] = useState<boolean>(true);
   const [isPublic, setIsPublic] = useState<boolean>(true);
-  const [inputText, setInputText] = useState<string>("");
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [letterIndex, setLetterIndex] = useState<string>("");
 
-  //바로보내기 or 예약보내기 버튼 활성화
+  // 바로보내기 or 예약보내기 버튼 활성화
   const SendToggleButton = (isSendImmediate: boolean) => {
     setIsSendImmediate(isSendImmediate);
     if (!isSendImmediate) {
       setSelectedDate(new Date());
     }
   };
-  //공개 or 비공개 버튼 활성화
+
+  // 공개 or 비공개 버튼 활성화
   const ToggleButton = (isPublic: boolean) => {
     setIsPublic(isPublic);
   };
 
-  const inputTextHandler = (e: any) => {
-    setInputText(e.target.value);
-    console.log(inputText);
+  // 태그 클릭 시 동작
+  const TagHandler = (clickedTag: string) => {
+    setSelectedTag((prevSelectedTag) =>
+      prevSelectedTag === clickedTag ? null : clickedTag,
+    );
   };
 
-  //보내기버튼
+  //편지내용 공유 동작
+  const onLetterIndexChange = (text: string) => {
+    setLetterIndex(text);
+  };
+
+  // 보내기버튼
   const SendInformationHandler = () => {
-    const userInformatoin = {
+    const userInformation = {
+      tags: selectedTag ? [selectedTag] : [],
       isSendImmediate,
       isPublic,
-      inputText,
       selectedDate,
+      letterIndex,
     };
-    console.log(userInformatoin);
-    setInputText("");
+    console.log(userInformation);
   };
 
   return (
     <>
       <SOptionBox>
-        <h1>신년인사, 연애, 모두에게 버튼태그 만들기</h1>
+        <div>
+          {tags.map((tag) => (
+            <SPublicButton
+              key={tag}
+              onClick={() => TagHandler(tag)}
+              active={selectedTag === tag}
+            >
+              {tag}
+            </SPublicButton>
+          ))}
+        </div>
         <SPublicButton
           onClick={() => SendToggleButton(true)}
           active={isSendImmediate}
@@ -64,14 +86,7 @@ const OptionBox = () => {
         <SPublicButton onClick={() => ToggleButton(false)} active={!isPublic}>
           익명으로 공개
         </SPublicButton>
-        <div>
-          <h3>발신할 곳</h3>
-          <input
-            value={inputText}
-            onChange={inputTextHandler}
-            placeholder="보낼곳을 적어주세요"
-          />
-        </div>
+        <div></div>
         <SSendButton onClick={SendInformationHandler}>보내기</SSendButton>
       </SOptionBox>
     </>
@@ -86,7 +101,7 @@ const SOptionBox = styled.div`
 
 const SPublicButton = styled.button<{ active: boolean }>`
   border: 0.1px solid;
-  background-color: ${(props) => (props.active ? "#00f" : "#fff")};
+  background-color: ${(props) => (props.active ? "#90c3ff" : "#fff")};
   color: ${(props) => (props.active ? "#fff" : "#000")};
 `;
 
@@ -94,4 +109,5 @@ const SSendButton = styled.button`
   border: 0.1px solid;
   width: 10vw;
 `;
+
 export default OptionBox;
